813e56e929d37c737fe7b6103d051f2c62e8a71a
Eusebiu <timoftesebi98@gmail.com>
2020-05-16 13:46:18 +0300

    asd

--- 07_C_3_keyboard.cpp
+++ 07_C_3_keyboard.cpp
@@ -1,13 +1,114 @@
 //SURSA:  lighthouse3D:  http://www.lighthouse3d.com/tutorials/glut-tutorial/keyboard-example-moving-around-the-world/ 
 
+#include <windows.h>
+#include <iostream>
+#include <stdlib.h>
 #include<gl/freeglut.h>
 #include<math.h>
+#include "SOIL.h"
+#include <GL/glut.h>
 // angle of rotation for the camera direction
 float angle=0.0;
 // actual vector representing the camera's direction
 float lx=0.0f,lz=-1.0f;
 // XZ position of the camera
 float x=0.0f,z=5.0f;
+
+/*	Create checkerboard texture	*/
+#define	checkImageWidth 64
+#define	checkImageHeight 64
+static GLubyte checkImage[checkImageHeight][checkImageWidth][4];
+GLubyte image[3 * checkImageWidth * checkImageHeight];
+GLuint texture1;
+
+static GLuint texName;
+
+
+
+void LoadTexture(void)
+{
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);	// Set texture wrapping to GL_REPEAT
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	int width, height;
+	unsigned char* image = SOIL_load_image("text_smiley_face.png", &width, &height, 0, SOIL_LOAD_RGB);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	// SOIL_free_image_data(image);
+	// glBindTexture(GL_TEXTURE_2D, 0);
+
+}
+
+void LoadTexture2(void)
+{
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);	// Set texture wrapping to GL_REPEAT
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	int width, height;
+	unsigned char* image = SOIL_load_image("text_earth.bmp", &width, &height, 0, SOIL_LOAD_RGB);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	// SOIL_free_image_data(image);
+	// glBindTexture(GL_TEXTURE_2D, 0);
+
+}
+
+
+void makeCheckImage(void)
+{
+	int i, j, c;
+
+	for (i = 0; i < checkImageHeight; i++) {
+		for (j = 0; j < checkImageWidth; j++) {
+			c = ((((i & 0x8) == 0) ^ ((j & 0x8)) == 0)) * 200;
+
+			checkImage[i][j][0] = (GLubyte)c;
+			checkImage[i][j][1] = (GLubyte)c;
+			checkImage[i][j][2] = (GLubyte)c;
+			checkImage[i][j][3] = (GLubyte)255;
+		}
+	}
+}
+
+
+void init(void)
+{
+	glShadeModel(GL_FLAT);
+	glEnable(GL_DEPTH_TEST);
+
+	makeCheckImage();
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	glGenTextures(1, &texName);
+	glBindTexture(GL_TEXTURE_2D, texName);
+
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, checkImageWidth, checkImageHeight,
+		0, GL_RGBA, GL_UNSIGNED_BYTE, checkImage);
+}
+
+void init2(void)
+{
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glShadeModel(GL_FLAT);
+}
+
 
 void changeSize(int w, int h)
 {
@@ -87,15 +188,17 @@ void renderScene(void) {
 			x+lx, 1.0f,  z+lz,
 			0.0f, 1.0f,  0.0f);
 
+	glEnable(GL_TEXTURE_2D);
+	LoadTexture();
         // Draw ground
 	glColor3f(0.9f, 0.9f, 0.9f);
 	glBegin(GL_QUADS);
-		glVertex3f(-100.0f, 0.0f, -100.0f);
-		glVertex3f(-100.0f, 0.0f,  100.0f);
-		glVertex3f( 100.0f, 0.0f,  100.0f);
-		glVertex3f( 100.0f, 0.0f, -100.0f);
+		glTexCoord2f(-100.0f, -100.0f); glVertex3f(-100.0f, 0.0f, -100.0f);
+		glTexCoord2f(-100.0f, 100.0f); glVertex3f(-100.0f, 0.0f, 100.0f);
+		glTexCoord2f(100.0f, 100.0f); glVertex3f(100.0f, 0.0f, 100.0f);
+		glTexCoord2f(100.0f, -100.0f); glVertex3f(100.0f, 0.0f, -100.0f);
 	glEnd();
-
+	glDisable(GL_TEXTURE_2D);
         // Draw 36 SnowMen
 	for(int i = -3; i < 3; i++)
 		for(int j=-3; j < 3; j++) {
@@ -169,7 +272,7 @@ int main(int argc, char **argv) {
 	glutInitWindowPosition(100,100);
 	glutInitWindowSize(320,320);
 	glutCreateWindow("Scena 3D cu oameni de zapada");
-
+	init2();
 	// register callbacks
 	glutDisplayFunc(renderScene);
 	glutReshapeFunc(changeSize);
